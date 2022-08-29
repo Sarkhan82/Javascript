d864dc1d468e8c17961852f63c9e40641e3d680d
Sarkhan <david.jonquieres@gmail.com>
2022-08-29 18:53:17 +0200

    exercice codewars

--- algo.js
+++ algo.js
@@ -13,3 +13,33 @@ function highAndLow(numbers) {
   numbers = numbers.split(" ").map(Number);
   return Math.max.apply(0, numbers) + " " + Math.min.apply(0, numbers);
 }
+
+// Kata de codewars
+
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+    z.push(i * x);
+  }
+  return z;
+}
+
+// autre kata
+
+function stringToArray(string) {
+  let result = string.split(" ");
+  return result;
+}
+
+//
+
+function countChar(string, char) {
+  let text = string.toLowerCase().split("");
+  let counter = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == char.toLowerCase()) {
+      counter++;
+    }
+  }
+  return counter;
+}
